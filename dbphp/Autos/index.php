2c8dcfd7919df65e9a90a819c456aa2737a9ac6b
crislav10 <cristianlav10@gmail.com>
2021-10-26 17:21:47 -0500

    POST>Redirect>GET, Login, Logout

--- dbphp/Autos/index.php
+++ dbphp/Autos/index.php
@@ -12,7 +12,11 @@
 </p>
 <p>
 Attempt to go to 
-<a href="autos.php">autos.php</a> without logging in - it should fail with an error message.
+<a href="view.php">view.php</a> without logging in - it should fail with an error message.
+<p>
+<p>
+Attempt to go to 
+<a href="add.php">add.php</a> without logging in - it should fail with an error message.
 <p>
 
 </div>
