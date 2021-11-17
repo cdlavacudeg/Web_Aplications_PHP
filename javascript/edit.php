96b4023e1d2c48c94832e04b3a280e70c4679ffb
crislav10 <cristianlav10@gmail.com>
2021-11-17 14:28:25 -0500

    Assigment Week 1

--- javascript/edit.php
+++ javascript/edit.php
@@ -7,54 +7,66 @@
         header('Location: index.php');
         return;
     }
-
-  
+    
     $stmt = $pdo->prepare("SELECT user_id,profile_id,first_name, last_name, email, headline, summary FROM profile WHERE profile_id=:pid");
     $stmt->execute(array(
                 ':pid' => htmlentities($_GET['profile_id']))
             );
-    $profile=$stmt->fetch();
-    $user_id=$profile['user_id'];
+    $profile=$stmt->fetch(PDO::FETCH_ASSOC);
+    $user_id=$profile["user_id"];
+    
+    if(isset($_POST['first_name']) || isset($_POST['last_name']) 
+        || isset($_POST['email']) || isset($_POST['headline']) 
+        || isset($_POST['summary'])||isset($_POST['profile_id'])){
+            error_log('revisando datos post');
+            if(strlen($_POST['first_name']) < 1 || strlen($_POST['last_name']) < 1 
+                || strlen($_POST['email']) < 1 || strlen($_POST['headline']) < 1 ){
 
-    if(isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['email']) && isset($_POST['headline']) && isset($_POST['summary'])){
-        if(strlen($_POST['first_name']) < 1 || strlen($_POST['last_name']) < 1 || strlen($_POST['email']) < 1 || strlen($_POST['headline']) < 1 ){
-            $_SESSION['messague']="All fields are required";
-            header('Location:edit.php?profile_id='.$_POST['profile_id']);
-            return;
-        }else {
-            if(!str_contains($_POST['email'],'@')){
-                $_SESSION['messague']="Email must have an at-sign (@)";
-                header('Location:edit.php?profile_id='.$_POST['profile_id']);
-                return;
-            }else{
-                
-                if($user_id = $_SESSION['user_id']){
-                    $stmt = $pdo->prepare(' UPDATE profile SET first_name=:fn, last_name=:ln, email=:em, headline=:he, summary=:su WHERE profile_id=:pid');
-                    $stmt->execute(array(
-                        ':fn' => $_POST['first_name'],
-                        ':ln' => $_POST['last_name'],
-                        ':em' => $_POST['email'],
-                        ':he' => $_POST['headline'],
-                        ':su' => $_POST['summary'],
-                        ':pid'=> $_POST['profile_id'])
-                    );
-                    $_SESSION['success']='Profile updated';
-                    header("Location:index.php");
-                    exit;
+                    $_SESSION['messague']="All fields are required";
+                    header('Location:edit.php?profile_id='.$_POST['profile_id']);
+                    return;
+            }else {
+                if(!str_contains($_POST['email'],'@')){
+                    $_SESSION['messague']="Email must have an at-sign (@)";
+                    header('Location:edit.php?profile_id='.$_POST['profile_id']);
+                    return;
                 }else{
-                    $_SESSION['error']="You don't own this entry";
-                    header('Location:index.php');
-                    return; 
+
+                    if($_POST['user_id'] == $_SESSION['user_id']){
+                        $stmt = $pdo->prepare(' UPDATE profile SET first_name=:fn, last_name=:ln, email=:em, headline=:he, summary=:su WHERE profile_id=:pid');
+                        $stmt->execute(array(
+                            ':fn' => htmlentities($_POST['first_name']),
+                            ':ln' => htmlentities($_POST['last_name']),
+                            ':em' => htmlentities($_POST['email']),
+                            ':he' => htmlentities($_POST['headline']),
+                            ':su' => htmlentities($_POST['summary']),
+                            ':pid'=> htmlentities($_POST['profile_id']))
+                        );
+                        error_log('Cambiando los datos');
+                        $_SESSION['success']='Profile updated';
+                        header("Location:index.php");
+                        exit;
+                    }else{
+                        $_SESSION['error']="You don't own this entry";
+                        header('Location:index.php');
+                        return; 
+                    }
+                    
                 }
-                
             }
         }
-    }
 
-    if($profile==false){
+    if($profile===false){
+        error_log('Error value profile_id');
+        $_SESSION['error']="Bad value of profile_id";
         header('Location: index.php');
-        exit;
+        return;
+  
     }
+    
+    
+
+    
  
 ?>
 
@@ -90,6 +102,7 @@
         
         <p>
         <input type="hidden" name="profile_id" value="<?= $profile['profile_id'] ?>">
+        <input type="hidden" name="user_id" value="<?= $profile['user_id'] ?>">
         <input type="submit" value="Save">
         <input type="submit" name="cancel" value="Cancel">
         </p>
